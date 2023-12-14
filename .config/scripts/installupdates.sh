d84bad9e959e70b91c7128a2ae61fc17a45f34c4
Taylan Ünver <taylanunver@hotmail.com>
2023-12-14 22:27:36 +0100

    screenshot

--- .config/scripts/installupdates.sh
+++ .config/scripts/installupdates.sh
@@ -10,6 +10,30 @@
 # Required: yay trizen timeshift btrfs-grub
 # -----------------------------------------------------
 
+sleep 1
+clear
+
+cat <<"EOF"
+ _   _           _       _
+| | | |_ __   __| | __ _| |_ ___  ___
+| | | | '_ \ / _` |/ _` | __/ _ \/ __|
+| |_| | |_) | (_| | (_| | ||  __/\__ \
+ \___/| .__/ \__,_|\__,_|\__\___||___/
+      |_|
+
+EOF
+
+_isInstalledYay() {
+    package="$1";
+    check="$(yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
 # ------------------------------------------------------
 # Confirm Start
 # ------------------------------------------------------
@@ -53,7 +77,7 @@ echo "Start update"
 echo "-----------------------------------------------------"
 echo ""
 
-packer -Syu
+yay
 
 notify-send "Update complete"
 
